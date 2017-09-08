0ab5310eae609d4bc7abc1e00816abd7c260def4
Nikita Sasin <nikita.sasin@hiqo-solutions.com>
2017-09-08 15:18:12 +0300

    Add collision test

--- src/js/main.js
+++ src/js/main.js
@@ -10,18 +10,18 @@
     canvas.width = mapSizeX * cellSize;
     canvas.height = mapSizeY * cellSize;
 
-    var map = ([
+    var gameMap = ([
       [1,1,1,1,1,1,1,1,1,1,1,1],
       [1,0,0,0,0,0,1,1,1,1,1,1],
-      [1,0,1,1,1,0,1,0,0,0,0,0],
+      [1,0,1,0,1,0,1,0,0,0,0,0],
       [1,0,1,0,1,0,1,0,1,1,1,1],
-      [1,0,1,0,1,0,1,0,1,0,0,1],
+      [1,0,1,0,1,0,1,0,0,0,0,1],
       [1,0,1,0,1,0,1,0,1,0,0,1],
       [1,0,1,0,1,0,0,0,1,0,0,1],
       [1,0,1,0,1,1,1,1,1,0,0,1],
+      [1,0,1,0,1,0,0,0,0,0,0,1],
       [1,0,1,0,0,0,0,0,0,0,0,1],
-      [1,0,1,0,0,0,0,0,0,0,0,1],
-      [1,0,1,0,0,0,0,0,0,0,0,1],
+      [1,0,1,0,1,0,0,0,0,0,0,1],
       [1,0,1,1,1,1,1,1,1,1,1,1],
     ]);
 
@@ -44,7 +44,7 @@
 
       for (var y = 0; y < mapSizeY; y++) {
         for (var x = 0; x < mapSizeX; x++) {
-          switch (map[y][x]) {
+          switch (gameMap[y][x]) {
             case 0:
               break;
             case 1:
@@ -61,24 +61,39 @@
 
     window.addEventListener('keydown', function (e) {
       console.log(e.keyCode);
+      var massX = tankObj.x / cellSize;
+      var massY = tankObj.y / cellSize;
+      console.log(massX + ' ' + massY);
+      console.log(gameMap);
       switch (e.keyCode) {
-        case 87:
-          tankObj.y -= cellSize;
+        case 87: //up
+          if(!gameMap[massY - 1][massX] && (gameMap[massY - 1][massX] >= 0)) {
+            tankObj.y -= cellSize;
+          }
           tankObj.i = 2;
           draw();
           break;
-        case 68:
-          tankObj.x += cellSize;
+
+        case 68: //right
+          if(!gameMap[massY][massX + 1] && gameMap[massY][massX + 1] <= gameMap[massY].length) {
+            tankObj.x += cellSize;
+          }
           tankObj.i = 0;
           draw();
           break;
-        case 83:
-          tankObj.y += cellSize;
+
+        case 83: //down
+          if(!gameMap[massY + 1][massX] && gameMap[massY + 1][massX] <= gameMap.length) {
+            tankObj.y += cellSize;
+          }
           tankObj.i = 3;
           draw();
           break;
-        case 65:
-          tankObj.x -= cellSize;
+
+        case 65: //left
+          if(!gameMap[massY][massX - 1] && gameMap[massY][massX - 1] >= 0) {
+            tankObj.x -= cellSize;
+          }
           tankObj.i = 1;
           draw();
           break;
